8747e02075eb2a14f9c9b04a15a11c3a037408aa
Srikanth_Sarma <vepasrikanthsarma@gmail.com>
2023-06-13 18:27:00 +0530

    Learning React Routing completed cleaned up all the code

--- src/Pages/ItemDetails.js
+++ src/Pages/ItemDetails.js
@@ -3,9 +3,7 @@ import { useParams } from "react-router-dom";
 
 function Item() {
     const {id} = useParams()
-    // const {id} = params
 
-    console.log(id)
     return (
         <div className="Item">
             <h1>ITEM {id}</h1>
--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -2,33 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 function Shop() {
-
     const [dailyItems, setdailyItems] = useState([])
-    const [shopItems, setshopItems] = useState([])
-
 
     useEffect(() => {
         fetchItems()
     }, [])
-
-    useEffect(() => {
-        dailyItems.map((item) => {
-            let temp = shopItems
-            temp.push(item.items[0])
-            setshopItems(temp)
-            // console.log(item.items[0])
-        },
-        // console.log(shopItems)
-    )
-
-    }, [dailyItems])
-
 
     const fetchItems = async () => {
         const data = await fetch('https://fortnite-api.com/v2/shop/br');
         const items = await data.json()
         setdailyItems(items.data.daily.entries)
     }
+    
     return (
         <div>
             <h1>SHOP</h1>
